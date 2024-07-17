411e2895e2c18b9595a2eade1919adcac2e0d326
Mayo-27 <nicolechua101@gmail.com>
2024-07-17 17:43:05 +0800

    Add files via upload

---
+++ Bank_Basic_Structure/src/main/java/org/example/Authenticate.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Authenticate {
+    String username = "cchua@gmail.com";
+    String password = "password";
+    boolean correctLogin = false;
+
+    Scanner scan = new Scanner(System.in);
+
+    void setCorrectLogin() {
+        do {
+            System.out.println("Enter Username: ");
+            username = scan.nextLine();
+
+            System.out.println("Enter Password: ");
+            password = scan.nextLine();
+
+            if (username.equals(username) && password.equals(password)) {
+                correctLogin = true;
+                System.out.println("You have entered the correct login info");
+                break;
+            } else {
+                System.out.println("Your login info was incorrect, please try again");
+            }
+        } while (correctLogin);
+
+        System.out.println("You are now logged in");
+    }
+}
